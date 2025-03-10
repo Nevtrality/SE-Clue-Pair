3e545fb637d62719f23a49a2151d20d9fa3edb12
cMillham <chloemillham@gmail.com>
2025-03-09 23:39:45 -0600

    updated custom exception and added exception tests

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,5 +1,11 @@
 package clueGame;
 
 public class BadConfigFormatException extends Exception{
+	BadConfigFormatException(){
+		super("Invalid board format");
+	}
 
+	BadConfigFormatException(String reason){
+		super(reason);
+	}
 }
