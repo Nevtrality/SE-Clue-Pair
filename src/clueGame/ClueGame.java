6efe1279df398a488c18b8e91bd9b6ef09c7c74d
cMillham <chloemillham@gmail.com>
2025-04-28 23:21:32 -0600

    added music and image for extra credit

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -1,11 +1,18 @@
 package clueGame;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JOptionPane;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.*;
+
 
 
 
@@ -36,6 +43,23 @@ public class ClueGame extends JFrame{
 		return board;
 	}
 	
+	public static void playSong() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+	    File song = new File("data/HouseTheme.wav");
+	    AudioInputStream audioInput = AudioSystem.getAudioInputStream(song);
+	    
+	    Clip songClip = AudioSystem.getClip();
+	    songClip.open(audioInput);
+	    songClip.start();
+	    
+	}
+	
+	public static void jumpscare() {
+		ImageIcon icon = new ImageIcon("data/housejumpscare.jpeg");
+        JOptionPane.showMessageDialog(
+                null,
+                new JLabel("Where's my Vicodin", icon, JLabel.LEFT));
+	}
+	
 	private JPanel drawControlPanel() {
 		return new GameControlPanel(board);
 	}
@@ -51,12 +75,17 @@ public class ClueGame extends JFrame{
 		// initialize game's frame
 		ClueGame gameFrame = new ClueGame();
 		JOptionPane.showMessageDialog(null, "<html><center>You are House<center><html>\n"+"<html><center>Figure out how the patient<center><html>\n"+"<html><center>died before your stupid collegues<center><html>", "Welcome to House",JOptionPane.INFORMATION_MESSAGE);
+		jumpscare();
 		// set default game frame size
 		gameFrame.setSize(900,900);
 		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gameFrame.setVisible(true);
 		gameFrame.setTitle("Clue Game");
 		// ideal board size(multiple of 28)x(multiple of 30)
+		
+		try {
+			playSong();
+		} catch(Exception e) {}
 		
 		gameFrame.revalidate();
 		gameFrame.repaint();
